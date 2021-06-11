dd8335e7964d7833ad294170c078d335bfbc2553
unknown <ronnie.michayev@gmail.com>
2021-06-11 18:32:46 +0300

    s

--- js/vod_main.js
+++ js/vod_main.js
@@ -68,9 +68,11 @@ const renderAllMovies = (data_ar) => {
 
 const compareValues = (key, order = "asc") => {
   return function innerSort(a, b) {
+  
+    
     if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0;
     const comparison = String(a[key]).localeCompare(String(b[key]));
-
+  
     return order === "desc" ? comparison * -1 : comparison;
   };
 };
